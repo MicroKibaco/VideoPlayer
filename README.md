c8c7ab7c421c6f373369427c0be4323758686bf4
MicroKibaco <yangzy3@asiainfo.com>
2017-04-10 21:39:30 +0800

    Added README file my first React-Native Project!

---
+++ README.md
@@ -0,0 +1,4 @@
+### ReactNative实战开发
+- ReactNative的生命周期
+
+<img src="images/react-native-life-cycle.png" ></img>
--- index.android.js
+++ index.android.js
@@ -1,53 +1,74 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * 解构赋值
  */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+/**
+ * JSX语法
+ */
 export default class VideoPlayer extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Asiainfo!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+
+        super(props);
+        this.state = {times: 0}
+
+    }
+
+    timesPlus() {
+        let times = this.state.times; // 获取当前状态的次数.
+        times++;
+        this.setState({
+                          times: times
+                      });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome} onPress={this.timesPlus.bind(this)}>
+                    有本事点我一下
+                </Text>
+                <Text style={styles.instructions}>
+                    你点了我{this.state.times}次
+                </Text>
+                <Text style={styles.instructions}>
+                    杨正友,哎呦不错哦
+                </Text>
+            </View>
+        );
+    }
 }
 
+/***
+ *CSS3 Flex布局
+ */
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+                                     container: {
+                                         flex: 1,
+                                         justifyContent: 'center',
+                                         alignItems: 'center',
+                                         backgroundColor: '#F5FCFF',
+                                     },
+                                     welcome: {
+                                         fontSize: 20,
+                                         textAlign: 'center',
+                                         margin: 10,
+                                     },
+                                     instructions: {
+                                         textAlign: 'center',
+                                         color: '#333333',
+                                         marginBottom: 5,
+                                     },
+                                 });
 
+/***
+ * 成功注册
+ */
 AppRegistry.registerComponent('VideoPlayer', () => VideoPlayer);
